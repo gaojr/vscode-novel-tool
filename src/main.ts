195a94ccee821d66fd1b63ae68999fa05b1d3267
gaojr <gaojr427@hotmail.com>
2020-04-27 12:39:40 +0800

    main从js转为ts

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,25 @@
-const vscode = require('vscode');
+import * as vscode from 'vscode';
+
+/**
+ * 缓存
+ */
+let disposables: vscode.Disposable[] = [];
 
 /**
  * this method is called when your extension is activated\
  * your extension is activated the very first time the command is executed
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+export function activate(context: vscode.ExtensionContext): void {
   // This code will only be executed once when your extension is activated
 }
 
 /**
  * this method is called when your extension is deactivated
  */
-function deactivate() {}
-
-module.exports = {
-  activate,
-  deactivate,
-};
+export function deactivate(): void {
+  if (!!disposables) {
+    disposables.forEach((item) => item.dispose());
+  }
+  disposables = [];
+}
