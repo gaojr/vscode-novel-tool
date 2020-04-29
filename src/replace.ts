8e0f0b186ef328334c6b9a18c87b2caa18c1bf55
gaojr <gaojr427@hotmail.com>
2020-04-29 15:09:49 +0800

    矫正注释

--- src/replace.ts
+++ src/replace.ts
@@ -1,10 +1,10 @@
 import { window, TextEditor, Range } from 'vscode';
 
 /**
- * 文字単位の変換
+ * 变换字符
  * @param {TextEditor} editor 编辑器
- * @param {RegExp} regEx
- * @param {number} offset
+ * @param {RegExp} regEx 正则表达式
+ * @param {number} offset 偏移量
  */
 function offsetCharCode(
   editor: TextEditor,
@@ -28,7 +28,7 @@ function offsetCharCode(
 }
 
 /**
- *全角英文转换为半角
+ * 全角转换为半角
  */
 export function full2Half(): void {
   if (window.activeTextEditor) {
